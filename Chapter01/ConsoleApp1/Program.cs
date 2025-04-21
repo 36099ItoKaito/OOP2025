4d0fb120c79b7b71129f9b59f6ccaa151216bcf7
infosys <infosys@C4223>
2025-04-21 09:57:48 +0900

    プロジェクト ファイルを追加します。

---
+++ Chapter01/ConsoleApp1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Diagnostics;
+
+namespace ProductSample {
+    internal class Program {
+        static void Main(string[] args) {
+
+            Product karinto = new Product(123, "かりんとう", 180);
+            Product Glock = new Product(124,"G17 gen5 MOS",19800);
+
+            //税抜きの価格を表示(かりんとうの税抜き価格は〇〇円です)
+            Console.WriteLine(karinto.Name + "の税抜き価格は" + karinto.Price + "円です");
+            //消費税額の表示(かりんとうの消費税額は〇〇円です)
+            Console.WriteLine(karinto.Name + "の消費税額は" + karinto.GetTax() + "円です");
+            //税込み価格の表示(かりんとうの税込み価格は〇〇円です)
+            Console.WriteLine(karinto.Name + "の税込み価格は" + karinto.GetPriceIncludingTax() + "円です");
+
+            //税抜きの価格を表示(G17の税抜き価格は〇〇円です)
+            Console.WriteLine(Glock.Name + "の税抜き価格は" + Glock.Price + "円です");
+            //消費税額の表示(G17の消費税額は〇〇円です)
+            Console.WriteLine(Glock.Name + "の消費税額は" + Glock.GetTax() + "円です");
+            //税込み価格の表示(G17の税込み価格は〇〇円です)
+            Console.WriteLine(Glock.Name + "の税込み価格は" + Glock.GetPriceIncludingTax() + "円です");
+        }
+    }
+}
---
+++ Chapter02/DistanceConverter/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace DistanceConverter {
+    internal class Program {
+        static void Main(string[] args) {
+
+            int i = int.Parse(args[1]);
+            int j = int.Parse(args[2]);
+
+            if (args.Length >= i && args[0] == "-tom") {
+                PrintFeetToMeterList(i, j);
+            } else {
+                PrintMeterToFeetList(i,j);
+
+            }
+        }
+
+        static void PrintFeetToMeterList(int i, int j) {
+            for (int feet = i; feet <= j; feet++) {
+                double meter = FeetToMeter(feet);
+                Console.WriteLine($"{feet}ft = {meter:0.0000}m");
+                }
+        }
+
+        static void PrintMeterToFeetList(int i, int j) {
+            for (int meter = i; meter <= j; meter++) {
+                double feet = FeetToMeter(meter);
+                Console.WriteLine($"{meter}ft = {feet:0.0000}m");
+            }
+        }
+
+        static double FeetToMeter(int feet) {
+            return feet * 0.3048;
+        }
+
+        static double MeterToFeet(int meter) {
+            return meter / 0.3048;
+        }
+    }
+}
