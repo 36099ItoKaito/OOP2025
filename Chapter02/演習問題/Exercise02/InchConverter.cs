f0edc9e6323860ec00c3584aade05b6f87bbb309
ItoKaito <oibc36099@ojs.ac.jp>
2025-04-23 10:55:16 +0900

    機能追加②完成

--- Chapter02/演習問題/Exercise02/InchConverter.cs
+++ Chapter02/演習問題/Exercise02/InchConverter.cs
@@ -4,11 +4,13 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Exercise02　{
-    class InchConverter　{
+namespace Exercise02 {
+    class InchConverter {
 
         //定数
         private const double ratio = 0.0254;
+
+        private const double ratio2 = 0.9144;
 
         public static double InchiToMeter(double inch) {
             return inch * ratio;
@@ -16,8 +18,14 @@ namespace Exercise02　{
 
         public static double MeterToInchi(double meter) {
             return meter / ratio;
+        }
 
+        public static double YardToMeter(double yard) {
+            return yard * ratio2;
+        }
 
+        public static double MeterToYard(double meter) {
+            return meter / ratio2;
         }
     }
 }
--- Chapter02/演習問題/Exercise02/Program.cs
+++ Chapter02/演習問題/Exercise02/Program.cs
@@ -1,8 +1,10 @@
-﻿namespace Exercise02 {
+﻿using System.Diagnostics.Metrics;
+
+namespace Exercise02 {
     internal class Program {
         static void Main(string[] args) {
 
-            //表示
+            /*//表示
             Console.WriteLine("***変換アプリ***");
             Console.WriteLine("1:インチからメートル");
             Console.WriteLine("2:メートルからインチ");
@@ -26,7 +28,35 @@
                     double inch = InchConverter.MeterToInchi(meter);
                     Console.WriteLine($"{meter}mt = {inch:0.0000}inch");
                 }
+            }*/
+
+            Console.WriteLine("***変換アプリ***");
+            Console.WriteLine("1:ヤードからメートル");
+            Console.WriteLine("2:メートルからヤード");
+
+            Console.Write(">");
+            int start2 = int.Parse(Console.ReadLine());
+
+            if(start2 == 1) {
+
+                Console.Write("変換前(ヤード)：");
+                int yard = int.Parse(Console.ReadLine());
+                double aa = InchConverter.YardToMeter(yard);
+                Console.Write("変換後(メートル)：" + $"{aa : 0.000}");
+
+            } else if(start2 == 2) {
+                Console.Write("変換前(メートル)");
+                int meter = int.Parse(Console.ReadLine());
+                double a = InchConverter.MeterToYard(meter);
+                Console.Write("変換後(ヤード)："+ $"{a: 0.000}");
             }
+
+            
+            
+
+
+
+
         }
     }
 }
