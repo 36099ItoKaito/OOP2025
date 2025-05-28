614b77ffb5ef7d4d22909cdc0fb6699251a8d4a9
ItoKaito <oibc36099@ojs.ac.jp>
2025-05-28 11:49:11 +0900

    7-2-1~7-1-7完成

---
+++ Chapter07/Section01/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace Section01 {
+    internal class Program {
+        static void Main(string[] args) {
+            var books = Books.GetBooks();
+
+            //本の平均金額を表示
+            Console.WriteLine((int)books.Average(x => x.Price));
+
+            //本のページ合計を表示
+            Console.WriteLine((int)books.Sum(x => x.Pages));
+
+            //金額の安い書籍名と金額を表示
+            var Price = books.Where(x => x.Price == books.Min(b => b.Price));
+            foreach(var item in Price) {
+                Console.WriteLine(item.Title + ":" + item.Price + "円");
+            }
+            //var Price = books.MinBy(b => b.Price);
+            //Console.WriteLine($"{Price.Title}:{Price.Price}円");
+
+            //ページが多い書籍名とページ数を表示
+            var Pages = books.Where(x => x.Pages == books.Max(b => b.Pages));
+            foreach (var item in Pages) {
+                Console.WriteLine(item.Title + ":" + item.Pages + "ページ");
+            }
+            //var Pages = books.MaxBy(b => b.Pages);
+            //Console.WriteLine($"{Pages.Title}:{Pages.Pages}ページ");
+
+            //タイトルに物語が含まれている書籍名をすべて表示
+            var titles = books.Where(x => x.Title.Contains("物語"));
+            foreach (var book in titles) {
+                Console.WriteLine(book.Title);
+            }
+        }
+    }
+}
