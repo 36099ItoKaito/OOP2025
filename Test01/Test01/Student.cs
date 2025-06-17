297999ce088000159a91a1676b2972eaca1554e0
ItoKaito <oibc36099@ojs.ac.jp>
2025-06-17 11:43:01 +0900

    設問１＿Studentクラス完成

--- Test01/Test01/Student.cs
+++ Test01/Test01/Student.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 namespace Test01 {
     public class Student {
         public string? Name { get; init; }
-        public string? Code { get; init; }
+        public string? Subject { get; init; }
         public int Score { get; init; }
 
 
