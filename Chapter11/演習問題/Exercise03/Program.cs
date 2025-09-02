f1b7fc659b8c075311f5fc01928efbb7d14f439c
ItoKaito <oibc36099@ojs.ac.jp>
2025-09-02 13:23:18 +0900

    11章まで終了

---
+++ Chapter11/演習問題/Exercise03/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Exercise03 {
+    internal class Program {
+        static void Main(string[] args) {
+            string[] texts = [
+                "Time is money.",
+                "What time is it?",
+                "It will take time.",
+                "We reorganized the timetable.",
+            ];
+
+            Regex regex = new Regex(@"\btime\b", RegexOptions.IgnoreCase);
+
+            foreach (string line in texts) {
+                MatchCollection matches = regex.Matches(line);
+
+                foreach (Match match in matches) {
+                    Console.WriteLine($"{line}: {match.Index}");
+                }
+            }
+
+            //foreach(var line in texts) {
+            //    var matches = Regex.Matches(line, @"\btime\b", RegexOptions.IgnoreCase);
+            //
+            //    foreach(Match match2 in matches) {
+            //        Console.WriteLine($"{line}, {match2.Index}");
+            //    }
+            //
+            //}
+        }
+    }
+}
---
+++ Chapter11/演習問題/Exercise04/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Exercise04 {
+    internal class Program {
+        static void Main(string[] args) {
+
+            var lines = File.ReadAllLines("sample.txt");
+
+            //var newlines = lines.Select(line => line.Replace("version=\"v4.0\"", "version=\"v5.0\"")).ToArray();
+
+            var newlines = lines.Select(s => Regex.Replace(s,@"\b[v|V]ersion\s*=\s*""v4\.0""", @"version=""v5.0"""));
+
+            File.WriteAllLines("sampleChange.txt", newlines);
+
+            //これ以降は確認用
+            var text = File.ReadAllText("sampleChange.txt");
+            Console.WriteLine(text);
+        }
+    }
+}
