1ee5568dcecddf97873b5ef56fb6cbe6b3d92df7
ItoKaito <oibc36099@ojs.ac.jp>
2025-10-02 14:30:04 +0900

    InotifyPropertyChangedの場合

---
+++ WPF/HelloWorld/ViewModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    class ViewModel : INotifyPropertyChanged
+    {
+        public ViewModel() {
+            ChangeMessageCommand = new DelegateCommand(
+                ()=> GreetingMessage = "ちぇいんそうめん");
+        }
+
+        private string _greetingMessage = "Hellow World";
+        public string GreetingMessage { 
+            get=>_greetingMessage;
+            set {
+                if(_greetingMessage != value) {
+                    _greetingMessage = value;
+                    PropertyChanged?.Invoke(
+                        this, new PropertyChangedEventArgs(nameof(GreetingMessage)));
+                }
+            }
+        }
+
+        public DelegateCommand ChangeMessageCommand { get; }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+    }
+}
