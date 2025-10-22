011bab1435be3660d39b5f8cc3901c2813dea053
ItoKaito <oibc36099@ojs.ac.jp>
2025-10-22 09:41:25 +0900

    デザイン

---
+++ SQLite/CustomerApp/MainWindow.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.ObjectModel;
+using System.IO;
+using System.Windows;
+using System.Windows.Media.Imaging;
+using Microsoft.Win32;
+using SQLite;
+
+namespace CustomerApp;
+
+public class Customer {
+    [PrimaryKey, AutoIncrement]
+    public int Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public string PhoneNumber { get; set; } = string.Empty;
+    public string Address { get; set; } = string.Empty;
+    public byte[]? ImageData { get; set; }
+}
+
+public partial class MainWindow : Window {
+
+    public MainWindow() {
+        
+    }
+}
