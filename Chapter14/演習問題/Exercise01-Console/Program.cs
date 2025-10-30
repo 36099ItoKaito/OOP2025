b3d5165849664a540840774cf3fc4db6d7cc440a
ItoKaito <oibc36099@ojs.ac.jp>
2025-10-30 15:58:32 +0900

    コンソール

---
+++ Chapter14/演習問題/Exercise01-Console/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+
+class Program {
+    static async Task Main(string[] args) {
+        string filePath = "走れメロス.txt";
+        await ReadFile(filePath);
+    }
+
+    static async Task ReadFile(string filePath) {
+        using (StreamReader reader = new StreamReader(filePath)) {
+            string line;
+            while ((line = await reader.ReadLineAsync()) != null) {
+                Console.WriteLine(line);
+            }
+        }
+    }
+}
