ffacb38a14b2bc0585d34af42ade6c2c9061d494
ItoKaito <oibc36099@ojs.ac.jp>
2025-11-17 13:35:19 +0900

    問題15.2完成

--- Chapter15/TemplateMethod/LineCounter/Program.cs
+++ Chapter15/TemplateMethod/LineCounter/Program.cs
@@ -9,7 +9,7 @@ namespace LineCounter {
                 filePath = args[0];
             } else {
                 Console.WriteLine("検索するファイルのパス：");
-                filePath = Console.ReadLine() ?? @"""C:\Users\infosys\source\repos\OOP2025\Chapter14\Section03""";
+                filePath = Console.ReadLine() ?? @"C:\Users\infosys\source\repos\OOP2025\Chapter13\Section01";
 
                 if (string.IsNullOrWhiteSpace(filePath)) {
                     Console.WriteLine("エラー: ファイルパスが入力されていません。");
